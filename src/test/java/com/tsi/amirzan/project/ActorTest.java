5da2a893a9816356ecb0dbec8a3b1a5e89a22c53
AmirzanJ <98361836+AmirzanJ@users.noreply.github.com>
2022-06-11 19:57:54 +0100

    Add actor unit tests

--- src/test/java/com/tsi/amirzan/project/ActorTest.java
+++ src/test/java/com/tsi/amirzan/project/ActorTest.java
@@ -14,8 +14,39 @@ public class ActorTest {
         Assertions.assertEquals(1, testActor.getActor_id(),"ID not found");
     }
 
-    public  void testSetActor_id(){
+    @Test
+    public void testSetActor_id(){
+        Actor testActor = new Actor("First","Last");
+        testActor.setActor_id(1);
+        Assertions.assertEquals(1,testActor.getActor_id(),"id not changed");
+        testActor.setActor_id(2);
+        Assertions.assertEquals(2,testActor.getActor_id(),"id not changed");
+    }
 
+    @Test
+    public void testGetFirstName(){
+        Actor testActor = new Actor("First","Last");
+        Assertions.assertEquals("First",testActor.getFirst_name(),"first name not found");
+    }
+
+    @Test
+    public void testSetFirstName(){
+        Actor testActor = new Actor("First","Last");
+        testActor.setFirst_name("Second");
+        Assertions.assertEquals("Second",testActor.getFirst_name(),"first name not changed");
+    }
+
+    @Test
+    public void testGetLastName(){
+        Actor testActor = new Actor("First","Last");
+        Assertions.assertEquals("Last",testActor.getLast_name(),"last name not found");
+    }
+
+    @Test
+    public void testSetLastName(){
+        Actor testActor = new Actor("First","Last");
+        testActor.setLast_name("NotLast");
+        Assertions.assertEquals("NotLast",testActor.getLast_name(),"last name not changed");
     }
 
 }
